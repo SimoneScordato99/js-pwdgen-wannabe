c1ac60fc7c15ee8cf8f3c766aafc9b276f7dc208
SimoneScordato99 <scordato.simone@yahoo.com>
2023-03-21 17:07:57 +0100

    tentativi

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,14 @@ let citta = prompt("in che città vivi?");
 let regione = prompt("in che regione è la tua città?");
 let born = prompt("quando sei nato?")
 let sex = prompt("sei maschio o femmina?")
+let regioneItaliana = [regione="abruzzo", regione="basilicata", regione="calabria", regione="campania", regione="emilia", regione="friuli", regione="lazio", regione="liguria", regione="lombardia", regione="marche", regione="molise", regione="piemonte", regione="puglia", regione="sardegna", regione="sicilia", regione="toscana", regione="trentino", regione="umbria", regione="aosta", regione="veneto"];
 
 
 document.getElementById("CardAction").innerHTML = `
 <h1>la tua password è ${nome}${cognome}${colore}21</h1>
 `;
 
-if (sex="maschio") {
+if (sex === "maschio") {
     document.getElementById("sesso").innerText =`o`
 } else {
     document.getElementById("sesso").innerText =`a`
@@ -33,10 +34,10 @@ document.getElementById("cognome").innerText =`${cognome}`
 document.getElementById("citta").innerText =`${citta}`
 document.getElementById("regione").innerText =`${regione}`
 
-if (regione="abruzzo","basilicata","calabria","campania","emilia","friuli","lazio","liguria","lombardia","marche","molise","piemonte","puglia","sardegna","sicilia","toscana","trentino","umbria","aosta","veneto") {
+if (regione === regioneItaliana) {
     document.getElementById("stato").innerText =`italia`
 } else {
-    document.getElementById("stato").innerText =`estero`
+    document.getElementById("stato").innerText =`?estero`
 }
 
 document.getElementById("born").innerText =`${born}`
